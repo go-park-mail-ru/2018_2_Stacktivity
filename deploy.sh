ae2ce19647301557c89e7c3012ddaabbfda0a4ff
Silvman <anoscillator@gmail.com>
2018-12-16 17:55:34 +0300

    deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@
 sudo mv /home/ubuntu/backend.conf /etc/nginx/conf.d/backend.conf
 sudo nginx -s reload
 cd /home/ubuntu/blep/back
-sudo docker-compose build
-sudo docker-compose restart
+sudo docker-compose up -d

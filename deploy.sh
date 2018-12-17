540563b7e9ac0491a09ce796907dea41dcaf7c78
ConstaConst <mitrkos@mail.ru>
2018-12-17 18:36:52 +0300

    delete nginx conf

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-sudo mv /home/ubuntu/backend.conf /etc/nginx/conf.d/backend.conf
-sudo nginx -s reload
 cd /home/ubuntu/blep/back
 sudo docker-compose down
 sudo docker-compose build
